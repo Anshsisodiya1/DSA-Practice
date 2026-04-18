771d21ae2e79b63dd3a29baf3de12f94e3c51eb7
Ansh Sisodiya <sisodiyaansh785@gmail.com>
2026-04-18 16:11:42 +0530

    Updated Basics of linked list insertion,deletion,middleand cycles in linked list

---
+++ Linkedlist/lec1.cpp
@@ -0,0 +1,15 @@
+// it is a linear data structure which is used to store the data in a linear way
+// it is a collection of nodes where each node contains data and a pointer to the next node
+// it is a dynamic data structure which means that it can grow and shrink in size at runtime
+// it is a non-contiguous data structure which means that the elements are not stored in contiguous memory locations
+// it is a self-referential data structure which means that it contains a pointer to itself
+
+// why ?? it is used to store the data in a linear way
+// it is used to implement other data structures like stacks, queues, etc.
+// it is used to implement dynamic memory allocation
+
+// Types of linked list
+// 1. Singly linked list   
+   
+// 2. Doubly linked list
+// 3. Circular linked list
